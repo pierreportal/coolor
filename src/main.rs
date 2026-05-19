667d7c600d8ad7dc3a0301c4ba24752a3e77d830
pierreportal <pierreportal.mac@gmail.com>
2026-05-19 23:02:11 +0200

    cleanup: clean cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod color_factory;
 use clap::Parser;
-use color_factory::{Color, ToString};
+use color_factory::Color;
 
 #[derive(Parser)]
 #[command(version, about)]
 struct Cli {
-    #[arg(short)]
-    p: Option<String>,
     #[arg(long)]
     hex: String,
 }
@@ -14,15 +12,7 @@ struct Cli {
 fn main() {
     let cli = Cli::parse();
     let hex = cli.hex;
-    let p = cli.p.unwrap_or("all".to_string());
     let color = Color::from_hex(&hex);
 
-    match p {
-        v if v == "rgb" => println!("{}", color.rgb.to_string()),
-        v if v == "hsl" => println!("{}", color.hsl.to_string()),
-        v if v == "lab" => println!("{}", color.lab.to_string()),
-        v if v == "lum" => println!("{}", color.luminance_wcag),
-        v if v == "all" => println!("{:#?}", color),
-        _ => println!("Invalid print format: {}", p),
-    };
+    println!("{:#?}", color);
 }
